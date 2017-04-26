f58e5821dee2c8101d9ae6bea36159c8eae8cbe4
juliascript <juliageist23@gmail.com>
2017-04-25 18:11:57 -0700

    starter code for projects that have cocoapods

---
+++ Pods/Target Support Files/Pods-xcconfigTutorial-withCocoapods/Pods-xcconfigTutorial-withCocoapods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xcconfigTutorial_withCocoapods : NSObject
+@end
+@implementation PodsDummy_Pods_xcconfigTutorial_withCocoapods
+@end
--- xcconfigTutorial-withCocoapods/ViewController.swift
+++ xcconfigTutorial-withCocoapods/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(Bundle.main.infoDictionary?["SAMPLE_API_KEY"] as? String)
     }
 
     override func didReceiveMemoryWarning() {
